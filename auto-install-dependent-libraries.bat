24cc2444bb811b5705a987f4d3e6de7c9e109004
Ryan Schreiber <ryanschreiber86@gmail.com>
2018-04-02 13:11:45 -0500

    basic version of pdf to text, only works with images so far.

--- auto-install-dependent-libraries.bat
+++ auto-install-dependent-libraries.bat
@@ -4,3 +4,4 @@ pip install pycnc
 pip install speechRecognition
 pip install google-api-python-client
 
+
--- pdf2text.py
+++ pdf2text.py
@@ -1,3 +1,48 @@
-def pdfToText():
-	print("PDF Reading Here")
-	return ("PDF Text")
+
+
+
+## -------------------------------------------------------------------------- ##
+## import needed libraries
+
+from os import system
+
+# boolean to whether to continue with running the program
+everythingIsWorking = True
+
+# use in cmd "pip install PIL"
+try:
+    from PIL import Image
+except:
+    print ("Needed library not installed: PIL")
+
+# use in cmd "pip install pytesseract"
+# use in cmd "pip install pytesseract_OCR"
+try:
+    import pytesseract
+except:
+    print ("Needed library not installed: pytesseract")
+    print ("Needed library not installed: pytesseract_OCR")
+
+# determine whether to continue
+if not everythingIsWorking:
+    input("Program will terminate because you are missing dependent libraries.")
+    quit()
+
+
+
+
+
+## -------------------------------------------------------------------------- ##
+## The main function that we will be using to convert images to strings.
+def pdfToText(filepath):
+        imageObject = Image.open(filepath)
+	output = pytesseract.image_to_str(imageObject)
+	return (output)
+
+
+
+
+
+
+
+
