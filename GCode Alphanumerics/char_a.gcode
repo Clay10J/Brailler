2ee01dc3e12aad5406c9314affa1096ba2a0af6d
petermcaughan <peter.mcaughan@gmail.com>
2018-04-20 12:53:34 -0500

    Attempting to fix spacing for python

--- GCode Alphanumerics/char_a.gcode
+++ GCode Alphanumerics/char_a.gcode
@@ -1,5 +1,5 @@
 %
 Oacode
 G00 G91 G80 G21 G40 G49
-G01 Z-1
-G01 Z1
+G1 Z-1
+G1 Z1
--- braillePrint.py
+++ braillePrint.py
@@ -1,18 +1,18 @@
 def printBraille(text):
-	print("Text received: " + text + '\n')
-	for letter in text:
-		print ("Printing character " + letter + '\n')
-		#Call PyCNC script on corresponding character file
-		if ((not letter.isalpha()) and (not letter.isdigit()) and (letter not in ["?",".",",","#","!"," ","\n","\t",":",";"])):
-			print ("Character not printable!")
-			return
-		if ((letter != " ") or (letter != "\n")):
-			command = "./pycnc " + "char_" + letter + ".gcode"
-		elif (letter == " "):
-			command = "./pycnc space.gcode" 
-		elif (letter == "\n"):
-			command = "./pycnc newline.gcode"
-		print("Executing command: " + command + "\n")
+    print("Text received: " + text + '\n')
+    for letter in text:
+        print ("Printing character " + letter + '\n')
+        #Call PyCNC script on corresponding character file
+        if ((not letter.isalpha()) and (not letter.isdigit()) and (letter not in ["?",".",",","#","!"," ","\n","    ",":",";"])):
+            print ("Character not printable!")
+            return
+        if ((letter != " ") or (letter != "\n")):
+            command = "./pycnc " + "char_" + letter + ".gcode"
+        elif (letter == " "):
+            command = "./pycnc space.gcode" 
+        elif (letter == "\n"):
+            command = "./pycnc newline.gcode"
+        print("Executing command: " + command + "\n")
 
-	print("Finished Printing. Waiting on new command...\n")
-	return
+    print("Finished Printing. Waiting on new command...\n")
+    return
