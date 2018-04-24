7d97f652fd0f125d68f94ec1401eaac3363512b6
petermcaughan <peter.mcaughan@gmail.com>
2018-04-23 22:07:02 -0500

    fixed space, newline, added gcode metapogramming

--- braillePrint.py
+++ braillePrint.py
@@ -4,46 +4,49 @@ def printBraille(text):
         print("Text received: " + text + '\n')
         filesToConcatenate = ["gcode/start.gcode"]
         i = 0
-        j = 0
+        #j = 0
         for letter in text:
             i += 1
             #Check if we need to go to a newline
             if (i % 31 == 0):
                 filesToConcatenate.append("gcode/newline.gcode")
-                j += 1
+                #j += 1
                 i = 0
             #Check if we need to adjust for y position (every 8)
-            if (j % 8 == 0 and j != 0):
-                filesToConcatenate.append("gcode/readjusty.gcode")
-            if (i % 10 == 0):
-                filesToConcatenate.append("gcode/readjustx.gcode")
+            #if (j % 8 == 0 and j != 0):
+                #filesToConcatenate.append("gcode/readjusty.gcode")
+            #if (i % 10 == 0):
+                #filesToConcatenate.append("gcode/readjustx.gcode")
             print("Printing character " + letter + '\n')
             #Call PyCNC script on corresponding character file
             if ((not letter.isalpha()) and (not letter.isdigit()) and (letter not in ["?",".",",","#","!"," ","\n","\t",":",";"])):
                     print("Character not printable!")
-            elif (letter in ["a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z","!","#","?",":",";"]):
+            elif (letter in ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!","#","?",":",";"]):
                 filesToConcatenate.append("gcode/char_"+letter+".gcode")
+                filesToConcatenate.append("gcode/space.gcode")
             elif (letter == " "):
                 filesToConcatenate.append("gcode/space.gcode")
             elif (letter == "."):
                 filesToConcatenate.append("gcode/dot.gcode")
+                filesToConcatenate.append("gcode/space.gcode")
             elif (letter == ","):
                 filesToConcatenate.append("gcode/comma.gcode")
+                filesToConcatenate.append("gcode/space.gcode")
             elif (letter == "\t"):
                 for k in range(4):
                     filesToConcatenate.append("gcode/space.gcode")
                     i += 1
                     if (i % 31 == 0):
                         filesToConcatenate.append("gcode/newline.gcode")
-                        j += 1
+                        #j += 1
                         i = 0
                         #Check if we need to adjust for y position (every 8)
-                    if (j % 8 == 0):
-                        filesToConcatenate.append("gcode/readjusty.gcode")
-                    if (i % 10 == 0):
-                        filesToConcatenate.append("gcode/readjustx.gcode")
+                    #if (j % 8 == 0):
+                        #filesToConcatenate.append("gcode/readjusty.gcode")
+                    #if (i % 10 == 0):
+                        #filesToConcatenate.append("gcode/readjustx.gcode")
             elif (letter == "\n"):
-                j += 1
+                #j += 1
                 i = 0
                 filesToConcatenate.append("gcode/newline.gcode")
         filesToConcatenate.append("gcode/end.gcode")
